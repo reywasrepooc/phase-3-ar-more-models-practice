cc9deef5dcc3b0c342707ff592560e998d94bd88
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-08-27 09:49:41 -0400

    finished first deliverable

---
+++ lecture_kit/db/migrate/20210827130457_create_plant_categories.rb
@@ -0,0 +1,9 @@
+class CreatePlantCategories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :plant_categories do |t|
+      t.integer :category_id
+      t.integer :plant_id
+    end
+    
+  end
+end
---
+++ lecture_kit/db/migrate/20210827130755_drop_category_id_from_plant.rb
@@ -0,0 +1,5 @@
+class DropCategoryIdFromPlant < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :plants, :category_id, :integer
+  end
+end
